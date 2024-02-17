bb8c20406ce0b988fcac64e421278fda33f0aa8b
Tiago Lima <tiagoo.lima15@gmail.com>
2024-02-17 14:32:58 -0300

    DbSet

--- APICatalog/Context/AppDbContext.cs
+++ APICatalog/Context/AppDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace APICatalog.Context
+﻿using APICatalog.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalog.Context;
+
+public class AppDbContext : DbContext
 {
-    public class AppDbContext
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+
 }
